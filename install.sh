8f6f90b81df0a265e4ba00addd17a65d364ea6fd
Mae Large <mlarge@vmware.com>
2024-01-23 06:20:17 -0700

    also moved data values to repo root, so kapp app from gitops install can reconcile all yamls in config subfolder

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 #set -o xtrace
 
 kapp deploy -a helm-supply-chain \
-  -f <(ytt -f config/supply-chain.yaml -f templates --data-values-file config/templates/supply-chain-values.yaml)
+  -f <(ytt -f config/supply-chain.yaml -f templates --data-values-file supply-chain-values.yaml)
 
